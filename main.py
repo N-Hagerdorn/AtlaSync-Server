081f8d3ff86a355912c1acebfd33b5aabe7c4730
N-Hagerdorn <n-hagerdorn@onu.edu>
2023-10-28 13:58:44 -0400

    Added virtual environment to .gitignore

--- main.py
+++ main.py
@@ -5,3 +5,16 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route('/room')
+def room():
+    # Get room
+    # Get floor from room.floor_id
+    # Get floor map
+    # Get building
+    # Get organization
+    return None
+
+@app.route('/floor')
+def floor():
+    return None
