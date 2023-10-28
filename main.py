ef7ecbb698220df29400d7009afbb14c32a826b1
N-Hagerdorn <n-hagerdorn@onu.edu>
2023-10-28 14:14:06 -0400

    Fixed Flask script.

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
 
 @app.route('/room')
 def room():
@@ -15,8 +17,10 @@ def room():
     # Get organization
     return None
 
-app.run()
 
 @app.route('/floor')
 def floor():
     return None
+
+
+app.run(host='192.168.0.5', port=5000, debug=True, threaded=False)
