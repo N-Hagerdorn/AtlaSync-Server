277d11ac9f27b26d59ae9f4a84c90596b9de1845
N-Hagerdorn <n-hagerdorn@onu.edu>
2023-10-28 19:22:20 -0400

    Fixed inconsistent naming.

--- db_model.py
+++ db_model.py
@@ -10,7 +10,7 @@ class Building:
     def __init__(self, building_id, name, organization_id):
         self.name = name
         self.uid = building_id
-        self.owner = organization_id
+        self.owner_id = organization_id
 
 
 class Floor:
@@ -18,7 +18,7 @@ class Floor:
     def __init__(self, floor_id, story, building_id):
         self.name = story
         self.uid = floor_id
-        self.owner = building_id
+        self.owner_id = building_id
 
 
 class Room:
@@ -26,5 +26,5 @@ class Room:
     def __init__(self, room_id, name, floor_id, location):
         self.name = name
         self.uid = room_id
-        self.owner = floor_id
+        self.owner_id = floor_id
         self.location = location
