77c8a1f16958cad3ee60dbf160271e3e2d3b09a4
N-Hagerdorn <n-hagerdorn@onu.edu>
2023-10-28 12:15:19 -0400

    Added QR code generator and groundwork for database class.

---
+++ database.py
@@ -0,0 +1,20 @@
+import mariadb
+
+
+class Database:
+
+    @classmethod
+    def addRoom(cls):
+        return None
+
+    @classmethod
+    def addFloor(cls):
+        return None
+
+    @classmethod
+    def addBuilding(cls):
+        return None
+
+    @classmethod
+    def addOrganization(cls):
+        return None
---
+++ qrCodeGenerator.py
@@ -0,0 +1,10 @@
+import segno
+
+
+class QRCodeGenerator:
+
+    @classmethod
+    def makeQRCode(cls, room_id):
+
+        qr_code = segno.make_qr(room_id)
+        qr_code.save(f'room_{room_id}_qr_code.png')
